d8fe478a6b0ed4c51457b850c37a20113818b2c5
Cifelli <robert_cifelli@comcast.com>
2022-09-16 16:31:59 -0400

    A4 Parts 1 and 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Cannot be blank")
+    @Size(max = 60, message = "Entry too long")
+    private String location;
 
+    public Employer() {}
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
